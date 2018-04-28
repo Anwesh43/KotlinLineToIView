42a59139ebcfaddfb3b52b4b4cab074474ed71ac
Anwesh43 <anweshthecool0@gmail.com>
2018-04-28 09:10:14 +0530

    adding linetoiview module

---
+++ linetoiview/src/main/java/ui/anwesome/com/linetoiview/LineToAView.kt
@@ -0,0 +1,10 @@
+package ui.anwesome.com.linetoiview
+
+/**
+ * Created by anweshmishra on 28/04/18.
+ */
+
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Paint.Cap.ROUND
