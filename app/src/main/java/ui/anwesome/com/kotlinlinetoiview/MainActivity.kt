a2a9aa56c388b8138c951c91f6c4ccc762193e7c
Anwesh43 <anweshthecool0@gmail.com>
2018-04-28 10:12:27 +0530

    adding View in activity

--- app/src/main/java/ui/anwesome/com/kotlinlinetoiview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinlinetoiview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlinlinetoiview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.linetoiview.LineToIView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LineToIView.create(this)
     }
 }
