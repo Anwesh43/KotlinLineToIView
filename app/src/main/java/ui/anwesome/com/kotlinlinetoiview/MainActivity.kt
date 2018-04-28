39311c14d9e461b7c10e68eedd0ff6f7b1fc34b8
Anwesh43 <anweshthecool0@gmail.com>
2018-04-28 10:29:27 +0530

    keeping screen on

--- app/src/main/java/ui/anwesome/com/kotlinlinetoiview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinlinetoiview/MainActivity.kt
@@ -1,5 +1,6 @@
 package ui.anwesome.com.kotlinlinetoiview
 
+import android.content.pm.ActivityInfo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         LineToIView.create(this)
         fullScreen()
     }
@@ -17,4 +19,5 @@ class MainActivity : AppCompatActivity() {
 fun MainActivity.fullScreen() {
     supportActionBar?.hide()
     window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 }
